29310d3a45e3edee5e719d3a24e07f539d348014
Julia <juliasuchomlyn@gmail.com>
2022-07-08 13:36:19 +0300

    add task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';  
+import  throttle  from 'lodash.throttle'; 
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+const onTime = function (data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+player.on('timeupdate', throttle((onTime), 1000));
+
+const newTime = localStorage.getItem(CURRENT_TIME);
+player.setCurrentTime(newTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
