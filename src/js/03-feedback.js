d1d9053a3919e867b4b6cf6dea6dcb901dea19d0
Julia <juliasuchomlyn@gmail.com>
2022-07-09 14:14:06 +0300

    add task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle'; 
 
+
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textArea: document.querySelector('.feedback-form textarea'),
+    inputMail: document.querySelector('.feedback-form input')
+};
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextAreaInput, 500));
+
+populateTextArea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    JSON.parse(localStorage.getItem(STORAGE_KEY));
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+function onTextAreaInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function populateTextArea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    if (savedMessage === null) {
+        return;
+    }
+        refs.textArea.value = savedMessage['message' || ''];
+        refs.inputMail.value = savedMessage['email' || ''];
+}
